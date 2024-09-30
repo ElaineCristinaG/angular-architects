05d46d1531f481683e966ebf8d6b409385aac374
Elaine <laine.c.g.carminhola@gmail.com>
2024-09-30 15:37:53 -0300

    :star: (app-routing) configuration on the routes for shell and mfe-books

--- projects/mfe-books/src/app/app-routing.module.ts
+++ projects/mfe-books/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksCatalogComponent } from './books-catalog/books-catalog.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'booksCatalog',
+    component: BooksCatalogComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent,
+  },
+  {
+    path:'booksCatalog',
+    loadChildren: () => loadRemoteModule({
+      remoteEntry: 'http://localhost:4200/remoteEntry.js',
+      remoteName: 'mfe-books',
+      exposedModule: './BooksCatalogModule'
+    }).then(m => m.BooksCatalogModule).catch(error => console.log(error, 'Error loading Book BooksCatalogModule'))
+  },
+  {
+    path:'publisher',
+    loadChildren: () => loadRemoteModule({
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      remoteName: 'mfe-publisher',
+      exposedModule: './Module'
+    }).then(module => module.AppModule).catch(error => console.log(error, 'Error loading Publisher AppModule'))
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
