8df1dce838b8c7341b9f282b70038b60e6309dcc
Elaine <laine.c.g.carminhola@gmail.com>
2024-09-30 15:46:07 -0300

    :star: (login) new component Login in shell

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BooksCatalogComponent } from '../../../mfe-books/src/app/books-catalog/books-catalog.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BooksCatalogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
