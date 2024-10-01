0b9faf21e24343a7e9818c07a695a43e8d222fad
Elaine <laine.c.g.carminhola@gmail.com>
2024-10-01 19:23:16 -0300

    :start: implementation form in login

--- projects/shell/src/app/login/login.component.ts
+++ projects/shell/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OrchestratorService } from '../services/orchestrator/orchestrator.service';
+import { AuthService } from '../services/auth/auth.service';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+  credentials: User = { authUser: '', authPass: '' };
+  loginFailed: boolean = false;
+
+  private orcService = inject(OrchestratorService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public registration(): void{
+    
+    this.orcService.formRegister.set(true);
+    console.log(this.orcService.formRegister())
+  }
+
+  public checkUser(): void{
+
+    this.credentials.authPass = this.password;
+    this.credentials.authUser = this.username;
+    console.log(this.credentials)
+    if (this.authService.login(this.credentials)) {
+      console.log(this.loginFailed, 'authTrue')
+        // this.router.navigate(['/booksCatalog']); 
+      } else {
+        this.loginFailed = true;
+        
+      }
+
+  }
+
+
+   
+ 
 
 }
