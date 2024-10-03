59b8477a7206194947560885294e6efb7d376fc7
Elaine <laine.c.g.carminhola@gmail.com>
2024-10-02 22:27:23 -0300

    :sponge: code clean code not used

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -1,26 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { BooksCatalogComponent } from '../../../mfe-books/src/app/books-catalog/books-catalog.component';
 import { RouterModule } from '@angular/router';
 import { InitComponent } from './init/init.component';
+import { HeaderComponent } from './header/header.component';
+import { provideHttpClient } from '@angular/common/http';
+import { AuthService } from './services/auth/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     InitComponent,
-    BooksCatalogComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    // ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    provideZoneChangeDetection(),
+    provideClientHydration(),
+    provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
