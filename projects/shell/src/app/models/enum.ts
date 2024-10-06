c4890eaf0f93e29e8f5e96a81bb685cc0e9a9d21
Elaine <laine.c.g.carminhola@gmail.com>
2024-10-06 17:39:28 -0300

    :star: added svg and jpg for edit book(modal component) crested enum messages

---
+++ projects/shell/src/app/models/enum.ts
@@ -0,0 +1,13 @@
+export enum ServiceResponseMessages {
+  CREATE_SUCCESS = 'Record successfully created.',
+  EDIT_SUCCESS = 'Record successfully updated.',
+  DELETE_SUCCESS = 'Record successfully deleted.',
+  TRY_AGAIN = 'Something went wrong, please try again.',
+  ERROR = 'An error occurred while processing your request. Please contact support.',
+}
+export enum TypeFormBook {
+   EDIT_FORM = 'Edit Book',
+   DELETE_FORM = 'Delete Form',
+   UPDATE_FORM = 'Update Form',
+   CREATE_FORM = 'Creat Form',
+}
